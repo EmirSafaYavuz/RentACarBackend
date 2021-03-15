1cdc1d8fb67b20b575351a6dcb3f938b3000be12
EmirSafaYavuz <emirsafayavuz@outlook.com>
2021-03-15 23:49:49 +0300

    IIS ERROR!

---
+++ Business/Abstract/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface ICategoryService
+    {
+        IResult Add(Category category);
+        IResult Update(Category category);
+        IResult Delete(Category category);
+        IDataResult<List<Category>> GetAll();
+    }
+}
